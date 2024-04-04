1994d6594369cbbd5bd02a570259b60fe2e329d5
Notiva <notiva@Notivas-MacBook-Pro.local>
2024-04-04 09:12:54 +0700

    materi pertemuan 3

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> <!-- struktur kepala -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Website untuk pembelajaran WEB">
     <title>TI 4A</title>
-</head>
-<body>
+</head> <!-- akhir struktur kepala -->
+<body> <!-- awal struktur tubuh (untuk membuat konten) -->
     <h1>Pemrograman WEB 1</h1>
-    <p>
-        Program Studi Teknik Informatika
+    
+    <p> <!-- awal paragraph -->
+        Daftar Materi Pemrograman Web 1, Program Studi Teknik Informatika:
         <ol type="1">
             <li>HTML Dasar</li>
             <li>Pembuatan FORM</li>
@@ -17,7 +19,6 @@
             <li>Pengiriman Data</li>
             <li>Templating</li>
         </ol>
-    </p>
-
-</body>
+    </p> <!-- akhir paragraph -->
+</body><!-- akhi struktur tubuh (untuk membuat konten) -->
 </html>
