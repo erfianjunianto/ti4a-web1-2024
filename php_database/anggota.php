07643e94d648dfb6be097bf367f1680e939cdc51
Notiva Indonesia <notivaindonesia@gmail.com>
2024-06-13 09:22:11 +0700

    update materi css

--- php_database/anggota.php
+++ php_database/anggota.php
@@ -1,41 +1,70 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="tampilan.css">
+    <title>Document</title>
+    <!-- <style>
+        h4 {
+            color:#50E0EE;
+            font-size:26px;
+            text-align:left;
+            font-family: 'Comic Sans Ms';
+        }
+        h2{
+            color:#0023FF;
+        }
 
-include ("koneksi.php");
-
-$query = mysqli_query($conn, "SELECT * FROM anggota ORDER BY id ASC");
-
-?>
-
-<h4>Daftar Anggota Perpustakaan</h4>
-<a href="form_register.php">Tambah Anggota Baru</a> <br><br><br>
-<table border="1" cellpadding="1">
-    <tr>
-        <th>No</th>
-        <th>Nama Anggota</th>
-        <th>Jenis Kelamin</th>
-        <th>Alamat</th>
-        <th>Status</th>
-        <th>Aksi</th>
-    </tr>
+        table, th, td{
+            border: 1px solid #0023FF;
+            font-family: "Arial Black";
+        }
+    </style> -->
+</head>
+<body>
     <?php
-    $no=1;
-    while($data = $query->fetch_object()){
-        ?>
-            <tr>
-                <td><?=$no;?></td>
-                <td><?=$data->nama_lengkap;?></td>
-                <td><?=$data->jenis_kelamin;?></td>
-                <td><?=$data->alamat;?></td>
-                <td><?=$data->status;?></td>
-                <td>
-                    <a href="form_edit_anggota.php?id=<?=$data->id;?>">Edit</a> |
-                    <a href="hapus_anggota.php?id=<?=$data->id;?>">Hapus</a>
-                </td>
-            </tr>
 
-        <?php
-        $no++;
-    }
+    include ("koneksi.php");
+
+    $query = mysqli_query($conn, "SELECT * FROM anggota ORDER BY id ASC");
 
     ?>
-</table>
+    <h2>Halaman</h2>
+    <h4>Daftar Anggota Perpustakaan</h4>
+
+    <a href="form_register.php">Tambah Anggota Baru</a> <br><br><br>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Nama Anggota</th>
+            <th>Jenis Kelamin</th>
+            <th>Alamat</th>
+            <th>Status</th>
+            <th>Aksi</th>
+        </tr>
+        <?php
+        $no=1;
+        while($data = $query->fetch_object()){
+            ?>
+                <tr>
+                    <td><?=$no;?></td>
+                    <td><?=$data->nama_lengkap;?></td>
+                    <td><?=$data->jenis_kelamin;?></td>
+                    <td><?=$data->alamat;?></td>
+                    <td><?=$data->status;?></td>
+                    <td>
+                        <a href="form_edit_anggota.php?id=<?=$data->id;?>">Edit</a> |
+                        <a href="hapus_anggota.php?id=<?=$data->id;?>">Hapus</a>
+                    </td>
+                </tr>
+
+            <?php
+            $no++;
+        }
+
+        ?>
+    </table>
+</body>
+</html>
